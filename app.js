8d90670f165312c0059345e8eb5d06266d090382
Camilla Alves <arcamilla42@gmail.com>
2025-11-27 18:38:19 -0300

    Beta Version

--- app.js
+++ app.js
@@ -11,57 +11,36 @@ const soundWin = new Audio(
 );
 soundWin.volume = 0.8;
 
-const TOTAL_RUNS_PLAYER = 19900;
-const API_OFFSET = 100;
-const BIOME_CHANGE_THRESHOLD = 2000;
-const SIMULATION_SPEED_MS = 125;
+const user = {
+    name: "",
+    id: "",
+    runs: 0,
+    games: new Set(),
+    categories: new Set(),
+    ils: 0,
+    full_game: 0,
+    levels: new Set(),
+    platforms: new Set(),
+    co_op_runs: 0,
+};
 
-let userId = "";
-let currentRunsLoaded = 0;
+const API_OFFSET = 100;
+const BIOME_CHANGE_THRESHOLD = 1000;
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
 let currentBiomeIndex = 0;
-let simulationInterval;
 
 const biomes = [
-    {
-        name: "Floresta",
-        url: "https://www.shutterstock.com/image-photo/mixed-forest-behind-green-grass-260nw-2455179617.jpg",
-    },
-    {
-        name: "Gelo",
-        url: "https://www.shutterstock.com/image-vector/arctic-landscape-vector-illustration-snowy-260nw-2535203055.jpg",
-    },
-    {
-        name: "Deserto",
-        url: "https://static.vecteezy.com/system/resources/thumbnails/001/786/598/large/far-west-desert-seamless-landscape-animation-loop-video.jpg",
-    },
-    {
-        name: "Rio De Janeiro",
-        url: "https://upload.wikimedia.org/wikipedia/commons/4/4b/Rio_de_Janeiro_Panoramic_from_P%C3%A3o_de_A%C3%A7%C3%BAcar_crop.jpg",
-    },
-    {
-        name: "Time",
-        url: "https://i.pinimg.com/originals/2c/5e/b4/2c5eb476123b15552bef158f90a86fd6.gif",
-    },
-    {
-        name: "Spaghetti",
-        url: "https://t3.ftcdn.net/jpg/03/48/33/20/360_F_348332074_9KJZhOZI7PzTLLp6HQTtVj9DkgHfGcLF.jpg",
-    },
-    {
-        name: "Space",
-        url: "https://www.shutterstock.com/shutterstock/videos/12663872/thumb/1.jpg",
-    },
-    {
-        name: "Pink Floyd",
-        url: "https://monkeybuzz.com.br/wp-content/uploads/2013/03/screenhunter_64-mar-01-1911.jpg",
-    },
-    {
-        name: "Underwater",
-        url: "https://static.vecteezy.com/system/resources/thumbnails/039/191/667/small_2x/an-underwater-view-of-a-coral-reef-with-fish-video.jpg",
-    },
-    {
-        name: "Bliss",
-        url: "https://upload.wikimedia.org/wikipedia/pt/2/27/Bliss_%28Windows_XP%29.png",
-    },
+    "https://www.shutterstock.com/image-photo/mixed-forest-behind-green-grass-260nw-2455179617.jpg",
+    "https://www.shutterstock.com/image-vector/arctic-landscape-vector-illustration-snowy-260nw-2535203055.jpg",
+    "https://static.vecteezy.com/system/resources/thumbnails/001/786/598/large/far-west-desert-seamless-landscape-animation-loop-video.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/4/4b/Rio_de_Janeiro_Panoramic_from_P%C3%A3o_de_A%C3%A7%C3%BAcar_crop.jpg",
+    "https://i.pinimg.com/originals/2c/5e/b4/2c5eb476123b15552bef158f90a86fd6.gif",
+    "https://t3.ftcdn.net/jpg/03/48/33/20/360_F_348332074_9KJZhOZI7PzTLLp6HQTtVj9DkgHfGcLF.jpg",
+    "https://www.shutterstock.com/shutterstock/videos/12663872/thumb/1.jpg",
+    "https://monkeybuzz.com.br/wp-content/uploads/2013/03/screenhunter_64-mar-01-1911.jpg",
+    "https://static.vecteezy.com/system/resources/thumbnails/039/191/667/small_2x/an-underwater-view-of-a-coral-reef-with-fish-video.jpg",
+    "https://upload.wikimedia.org/wikipedia/pt/2/27/Bliss_%28Windows_XP%29.png",
 ];
 
 const introScreen = document.getElementById("intro-screen");
@@ -73,15 +52,13 @@ const moaiTrigger = document.getElementById("moai-trigger");
 
 const displayPlayerName = document.getElementById("display-player-name");
 const runsCounterSpan = document.getElementById("runs-counter");
-const runsTotalSpan = document.getElementById("runs-total");
-const biomeNameSpan = document.getElementById("current-biome-name");
 const bgFront = document.getElementById("bg-front");
 const bgBack = document.getElementById("bg-back");
 const tableBody = document.getElementById("table-body");
 
-const apiBase = "https://www.speedrun.com/api/v1";
+const API_BASE = "https://www.speedrun.com/api/v1";
+const MAX_RUNS_PER_PAGE = 200;
 
-runsTotalSpan.innerText = TOTAL_RUNS_PLAYER;
 setupInitialBiomes();
 
 playerInput.addEventListener("focus", () => {
@@ -94,7 +71,19 @@ playerInput.addEventListener("keypress", (e) => {
 });
 
 async function validateAndStart() {
+    user.name = "";
+    user.id = "";
+    user.runs = 0;
+    user.games = new Set();
+    user.categories = new Set();
+    user.ils = 0;
+    user.full_game = 0;
+    user.levels = new Set();
+    user.platforms = new Set();
+    user.co_op_runs = 0;
     const name = playerInput.value.trim();
+    console.log(name);
+    user.name = name;
     if (!name) {
         alert("Por favor, digite o nome do player!");
         playerInput.focus();
@@ -102,13 +91,12 @@ async function validateAndStart() {
     }
 
     try {
-        const userUrl = `${apiBase}/users/${encodeURIComponent(playerInput)}`;
+        const userUrl = `${API_BASE}/users/${encodeURIComponent(user.name)}`;
         const userResponse = await fetch(userUrl);
         await sleep(1000);
 
         if (userResponse.status === 404) {
-            outputDiv.textContent = "User Not Found";
-            alert(`Error: Player "${playerId}" could not be found.`);
+            alert(`Error: Player "${user.name}" could not be found.`);
             playerInput.focus();
             return;
         }
@@ -120,11 +108,10 @@ async function validateAndStart() {
         }
 
         const userData = await userResponse.json();
-        const userId = userData.data.id;
+        user.id = userData.data.id;
     } catch (error) {
         console.error("Error fetching player runs:", error);
-        outputDiv.textContent = "ERROR";
-        alert("An unexpected error occurred. Check the console for details.");
+        alert(`Error: Player "${user.name}" could not be found.`);
         return;
     }
     soundMoai.pause();
@@ -136,42 +123,77 @@ function startGame() {
     introScreen.classList.add("hidden");
     gameScreen.classList.remove("hidden");
 
-    displayPlayerName.innerText = playerName;
+    displayPlayerName.innerText = user.name;
 
     soundRun.play().catch((e) => console.log("Audio bloqueado:", e));
-    simulationInterval = setInterval(simulateApiFetch, SIMULATION_SPEED_MS);
+    fetchAndAnimateRuns();
 }
 
 function setupInitialBiomes() {
-    bgFront.style.backgroundImage = `url('${biomes[0].url}')`;
-    bgBack.style.backgroundImage = `url('${biomes[1].url}')`;
-    biomeNameSpan.innerText = biomes[0].name;
+    bgFront.style.backgroundImage = `url('${biomes[0]}')`;
+    bgBack.style.backgroundImage = `url('${biomes[1]}')`;
 }
 
-function simulateApiFetch() {
-    currentRunsLoaded += API_OFFSET;
+async function fetchAndAnimateRuns() {
+    let offset = 0;
+    let keepFetching = true;
 
-    if (currentRunsLoaded >= TOTAL_RUNS_PLAYER) {
-        currentRunsLoaded = TOTAL_RUNS_PLAYER;
-        clearInterval(simulationInterval);
-        updateUI();
+    while (keepFetching) {
+        const runsUrl = `${API_BASE}/runs?status=verified&user=${user.id}&max=${MAX_RUNS_PER_PAGE}&offset=${offset}`;
 
-        setTimeout(showResults, 1000);
-    } else {
-        checkBiomeTransition();
-        updateUI();
+        try {
+            const runsResponse = await fetch(runsUrl);
+
+            await new Promise((resolve) => setTimeout(resolve, 500));
+
+            if (!runsResponse.ok) {
+                throw new Error(
+                    `API Fetch failed with status: ${runsResponse.status}`
+                );
+            }
+
+            const runsData = await runsResponse.json();
+            const runs = runsData.data;
+
+            user.runs += runs.length;
+            user.ils += runs.filter((run) => run.level !== null).length;
+            user.full_game += runs.filter((run) => run.level === null).length;
+            user.co_op_runs += runs.filter(
+                (run) => run.players.length > 1
+            ).length;
+
+            runs.forEach((run) => {
+                user.games.add(run.game);
+                user.categories.add(run.category);
+                user.levels.add(run.level);
+                user.platforms.add(run.platform);
+            });
+
+            checkBiomeTransition();
+            updateUI();
+
+            if (runs.length < MAX_RUNS_PER_PAGE) {
+                keepFetching = false;
+            } else {
+                offset += MAX_RUNS_PER_PAGE;
+            }
+        } catch (error) {
+            console.error("Erro durante a busca de runs:", error);
+            keepFetching = false;
+        }
     }
+
+    setTimeout(() => {
+        showResults();
+    }, 1000);
 }
 
 function updateUI() {
-    runsCounterSpan.innerText = currentRunsLoaded;
+    runsCounterSpan.innerText = user.runs;
 }
 
 function checkBiomeTransition() {
-    if (
-        currentRunsLoaded > 0 &&
-        currentRunsLoaded % BIOME_CHANGE_THRESHOLD === 0
-    ) {
+    if (user.runs > 0 && user.runs % BIOME_CHANGE_THRESHOLD === 0) {
         triggerBiomeFade();
     }
 }
@@ -187,10 +209,9 @@ function triggerBiomeFade() {
         bgFront.classList.remove("fade-out");
 
         currentBiomeIndex = nextBiomeIndex;
-        biomeNameSpan.innerText = nextBiome.name;
 
         const futureBiomeIndex = (currentBiomeIndex + 1) % biomes.length;
-        bgBack.style.backgroundImage = `url('${biomes[futureBiomeIndex].url}')`;
+        bgBack.style.backgroundImage = `url('${biomes[futureBiomeIndex]}')`;
     }, 200);
 }
 
@@ -204,23 +225,21 @@ function showResults() {
 }
 
 function populateTable() {
-    const fakeData = [
-        { cat: "Player", val: playerName },
-        { cat: "Status", val: "Lenda Viva" },
-        { cat: "Total Runs", val: TOTAL_RUNS_PLAYER.toLocaleString() },
-        { cat: "WRs Confirmados", val: Math.floor(Math.random() * 20) + 1 },
-        { cat: "Bioma Final", val: biomes[currentBiomeIndex].name },
-        { cat: "Tempo Jogado", val: "320 Horas" },
-        { cat: "Gato Preferido", val: "Maxwell" },
+    const userData = [
+        { cat: "Name", val: user.name },
+        { cat: "Runs", val: user.runs },
+        { cat: "Games", val: user.games.size },
+        { cat: "Categories", val: user.categories.size },
+        { cat: "ils-Runs", val: user.ils },
+        { cat: "Full-Game Runs", val: user.full_game },
+        { cat: "Levels", val: user.levels.size },
+        { cat: "Platforms", val: user.platforms.size },
+        { cat: "Co-op Runs", val: user.co_op_runs },
     ];
 
     tableBody.innerHTML = "";
-    fakeData.forEach((item) => {
+    userData.forEach((item) => {
         let row = `<tr><td>${item.cat}</td><td><strong>${item.val}</strong></td></tr>`;
         tableBody.innerHTML += row;
     });
 }
-
-/*
-
-*/
